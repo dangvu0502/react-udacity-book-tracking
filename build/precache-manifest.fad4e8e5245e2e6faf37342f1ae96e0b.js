c0382b5fde25224ddebbaed8eb9ad9cb36e939fd
unknown <dangvu0502@gmail.com>
2022-05-19 13:37:21 +0700

    Add starter filr

---
+++ build/precache-manifest.fad4e8e5245e2e6faf37342f1ae96e0b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "c2b52bf548fc37457a3d",
+    "url": "/static/css/main.4e61a539.chunk.css"
+  },
+  {
+    "revision": "c2b52bf548fc37457a3d",
+    "url": "/static/js/main.c2b52bf5.chunk.js"
+  },
+  {
+    "revision": "f68dcc1a0c0c6e129be0",
+    "url": "/static/js/1.f68dcc1a.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "20e8847d1eb7759c19b0e30beb8849cf",
+    "url": "/static/media/arrow-back.20e8847d.svg"
+  },
+  {
+    "revision": "9d4e3f366f039249695de85a30a02b95",
+    "url": "/static/media/arrow-drop-down.9d4e3f36.svg"
+  },
+  {
+    "revision": "75d390e5eabc3254418e950df631389b",
+    "url": "/static/media/add.75d390e5.svg"
+  },
+  {
+    "revision": "a8032675df203eecefd5c7013282ec97",
+    "url": "/index.html"
+  }
+];
