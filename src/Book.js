52b3c47f05dbaac4aac07f1a49c16c8e00239852
unknown <dangvu0502@gmail.com>
2022-05-23 08:29:07 +0700

    Fix Book image error

--- src/Book.js
+++ src/Book.js
@@ -1,30 +1,46 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Book extends Component {
- 
   render() {
     const book = this.props.book;
     return (
-        <div className="book">
-          <div className="book-top">
-            {book.imageLinks && book.imageLinks.thumbnail ?
-            <div className="book-cover" style={{ backgroundImage: `url("${book.imageLinks.thumbnail}")` }}></div>
-            : null}
-            <div className="book-shelf-changer">
-              <select>
-                <option value="none" disabled>Move to...</option>
-                <option value="currentlyReading">Currently Reading</option>
-                <option value="wantToRead">Want to Read</option>
-                <option value="read">Read</option>
-                <option value="none">None</option>
-              </select>
+      <div className="book">
+        <div className="book-top">
+          {book.imageLinks && book.imageLinks.thumbnail ? (
+            <div
+              className="book-cover"
+              style={{
+                width: 128,
+                height: 193,
+                backgroundImage: `url(${book.imageLinks.thumbnail})`,
+              }}
+            />
+          ) : (
+            <div>
+              <p>Image Error</p>
             </div>
+          )}
+          <div className="book-shelf-changer">
+            <select>
+              <option value="none" disabled>
+                Move to...
+              </option>
+              <option value="currentlyReading">Currently Reading</option>
+              <option value="wantToRead">Want to Read</option>
+              <option value="read">Read</option>
+              <option value="none">None</option>
+            </select>
           </div>
-          <div className="book-title">{book.title}</div>
-          <div className="book-authors">{book.authors ? book.authors.map((author) => <p key={author}>{author}</p>) : null}</div>
         </div>
+        <div className="book-title">{book.title}</div>
+        <div className="book-authors">
+          {book.authors
+            ? book.authors.map((author) => <p key={author}>{author}</p>)
+            : null}
+        </div>
+      </div>
     );
   }
-};
+}
 
 export default Book;
